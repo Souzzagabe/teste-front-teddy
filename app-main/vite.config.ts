a034987eb8c717d22a63a1474d7ad49410aa3a8e
souzzagab <gabriel.souza@lacreisaude.com.br>
2025-10-27 20:01:28 -0300

    fix: deploy error

--- app-main/vite.config.ts
+++ app-main/vite.config.ts
@@ -2,34 +2,35 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 import packageJson from "./package.json";
-// const deps = packageJson.dependencies as Record<string, string>;
 
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: "host_app",
-      remotes: {
-        remote_login: "http://localhost:5002/assets/remoteEntry.js",
-        remote_listing: "http://localhost:5003/assets/remoteEntry.js",
-      },
-      shared: {
-        react: {
-          requiredVersion: packageJson.dependencies.react,
+export default defineConfig(({ mode }) => {
+  const isDev = mode === "development";
+
+  return {
+    plugins: [
+      react(),
+      federation({
+        name: "host_app",
+        remotes: {
+          remote_login: isDev
+            ? "http://localhost:5002/assets/remoteEntry.js"
+            : "https://teste-front-teddy-login.vercel.app/assets/remoteEntry.js",
+          remote_listing: isDev
+            ? "http://localhost:5003/assets/remoteEntry.js"
+            : "https://teste-front-teddy-home.vercel.app/assets/remoteEntry.js",
         },
-           "react-dom": {
-          requiredVersion: packageJson.dependencies["react-dom"],
+        shared: {
+          react: { requiredVersion: packageJson.dependencies.react },
+          "react-dom": { requiredVersion: packageJson.dependencies["react-dom"] },
+          "react-router-dom": { requiredVersion: packageJson.dependencies["react-router-dom"] },
         },
-        "react-router-dom": {
-          requiredVersion: packageJson.dependencies["react-router-dom"],
-        },
-      },
-    }),
-  ],
-  build: {
-    modulePreload: false,
-    target: "esnext",
-    minify: false,
-    cssCodeSplit: false,
-  },
+      }),
+    ],
+    build: {
+      modulePreload: false,
+      target: "esnext",
+      minify: false,
+      cssCodeSplit: false,
+    },
+  };
 });
